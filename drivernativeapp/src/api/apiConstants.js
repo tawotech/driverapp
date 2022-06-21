efb1957483738d2338eec5d56e48aac88c78c0b6
ITS <tawonamushininga@gmail.com>
2022-06-21 10:46:07 +0200

    current app state

--- drivernativeapp/src/api/apiConstants.js
+++ drivernativeapp/src/api/apiConstants.js
@@ -1,6 +1,6 @@
 export default{
-    //url: 'https://www.etapath.com/api/v1'
+    url: 'https://www.etapath.com/api/v1'
     //url:'http://etapath-stage.herokuapp.com/api/v1' 
     //url:'http://10.30.1.45:3000/api/v1'
-    url:'http://192.168.0.101:3000/api/v1'
+    //url:'http://192.168.0.101:3000/api/v1'
 }
